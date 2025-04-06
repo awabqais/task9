91c1d42ea4741a97935d9e9c757abd591218abcd
awabqais <awab.qais@gmail.com>
2025-04-06 18:01:57 +0300

    commit#1
    in the NV on line 2, import class called ExchangeRateProvider from package Exchange. then on line 5 , declare privet object called exchangeRateProvider. then on line 7 and 8 create constructor.

--- task9/itcompany.converter/src/Salary/SalaryConverter.java
+++ task9/itcompany.converter/src/Salary/SalaryConverter.java
@@ -1,4 +1,10 @@
 package Salary;
+import Exchange.ExchangeRateProvider;
 
 public class SalaryConverter {
+    private final ExchangeRateProvider exchangeRateProvider;
+
+    public SalaryConverter(ExchangeRateProvider exchangeRateProvider) {
+        this.exchangeRateProvider = exchangeRateProvider;
+    }
 }
