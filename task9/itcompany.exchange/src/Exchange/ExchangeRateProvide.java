1eb369d6d81d566f729c2f2a1939ec3c3bc3757c
awabqais <awab.qais@gmail.com>
2025-04-06 17:38:51 +0300

    commit#1
    In the NV n line 4,5,6,7,8 and 9, create method has double as datatype called GetExchangeRate to convert the currency.

--- task9/itcompany.exchange/src/Exchange/ExchangeRateProvide.java
+++ task9/itcompany.exchange/src/Exchange/ExchangeRateProvide.java
@@ -1,4 +1,11 @@
 package Exchange;
 
 public class ExchangeRateProvide {
+    public double GetExchangeRate(String from, String to) {
+        if (from.equals("USD") && to.equals("EUR")) return 0.92;
+        if (from.equals("EUR") && to.equals("USD")) return 1.90;
+        if (from.equals("USD") && to.equals("UGX")) return 0.79;
+        if (from.equals("UGX") && to.equals("USD")) return 1.26;
+        return 1.0;
+    }
 }
