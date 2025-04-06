9a6b624aff2a1be6b4ee2a6435e84416da428c40
awabqais <awab.qais@gmail.com>
2025-04-06 19:35:23 +0300

    commit#1
    in the NV form line 8 , create instance for SalaryConverter  called SC. then on line 11 and 12, create an instance of class  ExchangeRateProvide .

--- task9/itcompany.converter/src/Salary/SalaryConverterTest.java
+++ task9/itcompany.converter/src/Salary/SalaryConverterTest.java
@@ -5,14 +5,15 @@ import Exchange.ExchangeRateProvide;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SalaryConverterTest {
-
+    private SalaryConverter SC;
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-       
+        ExchangeRateProvide exchangeRateProvider = new ExchangeRateProvide();
+        SC = new SalaryConverter(exchangeRateProvider);
     }
 
     @org.junit.jupiter.api.Test
     void convertSalary() {
-
+       
     }
 }
